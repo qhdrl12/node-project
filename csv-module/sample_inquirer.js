be6ebe801852ff6dd488f3019ab46cdf59abe266
BKLee <qhdrl12@gmail.com>
2018-08-26 16:18:32 +0900

    generator inquirer 작성 완료. output directory 이슈 존재

--- csv-module/sample_inquirer.js
+++ csv-module/sample_inquirer.js
@@ -1,43 +1,86 @@
 const fs = require('fs');
+const exec = require('child_process').exec;
 const inquirer = require('inquirer');
 
-// inquirer.prompt([{
-//     name: 'name',
-//     type: 'input',
-//     message: 'What\'s your name',
-// }, {
-//     name: 'iceCream',
-//     type: 'list',
-//     message: 'Which is your favorite of the following ice cream flavors?',
-//     choices: ['green tea', 'poppyseed jam', 'chile', 'vanilla'],
-//     default: 3
-// }]).then((answers) => {
-//     console.log(`\nHi ${answers.name}. I like ${answers.iceCream} ice cream too! \n`);
-// });
+const runInfo = require('./run-info.json');
 
 const hasExtension = (fileName, exts) => {
     return (new RegExp('(' + exts.join('|').replace(/\./g, '\\.') + ')$')).test(fileName);
 }
 
-let invalidExts = ['.json', '.md'];
-let baseDir = './';
-let files = fs.readdirSync(baseDir);
-let filterFiles = files.filter((file) => {
-    return !hasExtension(file, invalidExts);
-})
+const showValidFiles = (dir) => {
+    let files = fs.readdirSync(dir);
+    let filterFiles = files.filter(file => {
+        return !hasExtension(file, invalidExts);
+    });
 
-inquirer.prompt([{
-    name: 'file',
-    type: 'list',
-    message: 'What\'s your run file?',
-    choices: filterFiles,
-    default: 1
-}, {
-    name: 'iceCream',
-    type: 'list',
-    message: 'Which is your favorite of the following ice cream flavors?',
-    choices: ['green tea', 'poppyseed jam', 'chile', 'vanilla'],
-    default: 3
-}]).then((answers) => {
+    return filterFiles;
+}
+
+const inquirerListTypeConfig = (dir) => {
+    return {
+        name: 'file',
+        type: 'list',
+        message: 'What\'s your run files?',
+        choices: showValidFiles(dir),
+        default: 1
+    }
+}
+
+let invalidExts = ['.json', '.md'];
+let validName = ['index'];
+let baseDir = './';
+let nowConfig = runInfo;
+let runDir = baseDir;
+
+inquirer.prompt([
+    inquirerListTypeConfig(baseDir)
+]).then((answers) => {
+    nowConfig = nowConfig[answers.file];
+    runDir += answers.file + "/";
+
     console.log(`\nHi ${answers.file}. I like ${answers.iceCream} ice cream too! \n`);
+
+    let isDir = fs.lstatSync(baseDir + answers.file).isDirectory();
+
+    if (isDir) {
+        inquirer.prompt([
+            inquirerListTypeConfig(baseDir + answers.file)
+        ]).then((answers) => {
+            //파일의 경우 
+            let runFileName = runDir + answers.file;
+            nowConfig = nowConfig[answers.file];
+            console.log(`nowconfig : ${JSON.stringify(nowConfig)}`);
+
+            let inquirerInput = Object.keys(nowConfig).map(key => {
+                return {
+                    name: key,
+                    type: 'input',
+                    message: nowConfig[key]
+                }
+            });
+
+            inquirer.prompt(
+                inquirerInput
+            ).then(answers => {
+                console.log(`---run with options---`)
+
+                let options = Object.keys(nowConfig).map((key, idx) => {
+                    console.log(`[${idx}] ${key} : ${answers[key]}`);
+                    return `${key} ${answers[key]}`;
+                }).reduce((str, option) => {
+                    return str += ` ${option}`;
+                });
+
+                console.log(`file with options : node ${runFileName} ${options}`);
+                let runScript = `node ${runFileName} ${options}`;
+                exec(runScript, (error, stdout, stderr) => {
+                    console.log(`stdout : ${stdout}`);
+                });
+            });
+        });
+    } else {
+
+    }
+
 });
